549ee2d8f5e877fa25cb35974a2d348acd9069cb
wonder woman <ww@dccomics.com>
2020-08-20 22:29:27 +0100

    Update script.js
    For one episode, displayed the following:
    - the episode's name
    - the season number
    - the episode number
    - the episode's medium-sized image
    - the episode's summary text

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function makePageForEpisodes(episodeList) {
 }
 
 window.onload = setup;
+
+const episode = getOneEpisode();
+
+const bodyEl = document.querySelector("body");
+const containerEl = document.createElement("div");
+const nameEl = document.createElement("p");
+const seasonEl = document.createElement("p");
+const numberEl = document.createElement("p");
+const imageMediumEl = document.createElement("img");
+const summaryEl = document.createElement("p");
+
+nameEl.textContent = episode.name;
+seasonEl.textContent = episode.season;
+numberEl.textContent = episode.number;
+imageMediumEl.src = episode.image.medium;
+summaryEl.innerHTML = episode.summary;
+
+bodyEl.appendChild(containerEl);
+containerEl.appendChild(nameEl);
+containerEl.appendChild(seasonEl);
+containerEl.appendChild(numberEl);
+containerEl.appendChild(imageMediumEl);
+containerEl.appendChild(summaryEl);
