f62637b6ecedf7578e807387dbff23f877aa58dd
wonder woman <ww@dccomics.com>
2020-08-28 16:41:09 +0100

    moved some live search notes from JS to README.md. Moved allEpisodesContainerEl.innerHTML = "" to just before the filter method

--- script.js
+++ script.js
@@ -64,29 +64,18 @@ bodyEl.appendChild(allEpisodesContainerEl);
 const footerEl = document.querySelector("footer");
 bodyEl.appendChild(footerEl);
 
-// -- LIVE SEARCH with "keyup" -- custom search filter 8:11 https://www.youtube.com/watch?v=3NG8zy0ywIk
-// -- LIVE SEARCH with "fetch" -- Quick Autocomplete App With JS & JSON https://www.youtube.com/watch?v=1iysNUrI3lw
-// -- LIVE SEARCH with "keyup" + "filter" -- https://www.youtube.com/watch?v=DzXmAKdEYIs
-/*const filteredEpisodeList = searchEl.addEventListener("keyup", function(e) {
-  const term = e.target.value.toLowerCase();
-  episodes.filter(function(episode) {
-    if(episode.toLowerCase().includes(term) ||
-    episode.toLowerCase().includes(term)) {
-      makePageForEpisodes(filteredEpisodeList);
-    }
-  })
-})*/
+// -- LIVE SEARCH RESOURCES -- see README.md file
 
 searchEl.addEventListener("keyup", function(e) {
 
   let episodes = [];
   let searchValue = e.target.value.toLowerCase();
   const allEpisodes = getAllEpisodes();
-  allEpisodesContainerEl.innerHTML = "";
 
   if(searchValue === null) {
     episodes = allEpisodes;
   } else {
+    allEpisodesContainerEl.innerHTML = "";
     episodes = allEpisodes.filter(episode => (
       episode.name.toLowerCase().includes(searchValue) ||
       episode.summary.toLowerCase().includes(searchValue)
